f300225e172e8d4bae186b34f5a4da43848942e6
Manasvi Singh <singhmanasvi71@gmail.com>
2026-03-01 22:27:47 +0530

    commit

---
+++ conditional.js
@@ -0,0 +1,42 @@
+var age=20;
+if (age>18) { //if statement
+    document.write("<br>You are eligible to vote.</br>");
+    }
+
+    let age=20;
+    if (age>=18) { //if else statement
+        console.log("You are an adult.");
+
+    } else {
+        console.log("You are a minor.");
+    }
+    var grade='A'; //switch case
+    document.write("Entering switch block</br>");
+    switch (grade){
+        case 'A':
+            document.write("Good Job</br>");
+            break;
+    
+    case 'B':
+            document.write("Pretty Good</br>");
+            break;
+        case 'C':
+            document.write("Passed</br>");
+            break;
+            case 'D':
+            document.write("Not Good</br>");
+            break;
+            case 'F':
+            document.write("Failed</br>");
+            break;
+  default:
+    document.write("Unknown Grade</br>")
+    }
+    document.write("Exiting switch block.");
+
+    function myFunction() { //function
+        alert("Hello World!");
+    }
+    function sayHello(name, age) {
+        document.write(name+"is"+age+"years old.");
+    }
---
+++ loops.js
@@ -0,0 +1,17 @@
+var count; //for loops
+document.write("starting loop "+"<br/>");
+for (count=0;count<10;count++) {
+    document.write("current count:"+count);
+    document.write("<br/>");
+
+} document.write("loop stopped");
+document.write("<br/>");
+
+//while loop
+var count=0;
+document.write("Starting loop");
+ while (count<10) {
+    document.write("Current count:"+count+"<br/>");
+    count++;
+ }
+ document.write("Loop Stopped</br>");
